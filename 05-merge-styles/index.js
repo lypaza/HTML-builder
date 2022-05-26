f5102eb9e214474ab9c34327c38775439f17e467
German N <germannikitchenko@gmail.com>
2022-05-26 03:25:56 +0300

    fix: fix task 05

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,33 +2,45 @@ const path = require('path');
 const fs = require('fs');
 
 
+
+
 const dirOriginal = path.join(__dirname, '.', 'styles');
 const dirBundle = path.join(__dirname, './project-dist', 'bundle.css');
-//let arr = [];
+let arrResult = [];
 
 
+async function readDir() {
+  const files = await fs.promises.readdir(dirOriginal);
+  let arrFiles = [];
+  arrFiles.push(files);
+  readFile(arrFiles, arrResult);
+}
+
+async function readFile(arr, result) {
+  for (let files of arr) {
+    for (let filesOrig of files) {
+      if (path.extname(filesOrig) == '.css') {
+        let pathOrigFile = path.join(__dirname, './styles', filesOrig);
+        const dataFile = await fs.promises.readFile(pathOrigFile, {encoding: 'utf-8'});
+        console.log(filesOrig + ' записан в массив\n');
+        result += dataFile;
+        deleteOldBundle();
+        createNewBundle(result);
+      }
+    }
+  }
+}
 
 
-function copyDir() {
-  fs.promises.readdir(dirOriginal, {withFileTypes: true})
+async function deleteOldBundle() {
+  let unlinkStyle = path.join(__dirname, '.', 'project-dist');
+  fs.promises.readdir(unlinkStyle, {withFileTypes: true})
     .then(filenames => {
       for (let filename of filenames) {
+        let StyleFilepath = path.join(__dirname, './project-dist', filename.name);
         if (path.extname(filename.name) == '.css') {
-          console.log(filename.name);
-          let pathOrigFile = path.join(__dirname, './styles', filename.name);
-          //let readStream = fs.createReadStream(pathOrigFile, 'utf8');
-          //let writeStream = fs.createWriteStream(dirBundle);
-          //readStream.pipe(writeStream);
-          fs.readFile(pathOrigFile, 'utf8', function(error, fileContent){ 
-            if(error) throw error; 
-                 
-           
-            let arrB = [];
-            arrB += fileContent;
-            //console.log(arrB);
-            fs.writeFile(dirBundle, arrB, function(error){
-              if(error) throw error;
-            });
+          fs.unlink(StyleFilepath, () => {
+            console.log('Бандл актуализирован\n');
           });
         }
       }
@@ -38,5 +50,17 @@ function copyDir() {
     });
 }
 
+async function createNewBundle(data) {
+  fs.open(dirBundle, 'a', (err) => {
+    if(err) throw err;
+    fs.appendFile(dirBundle, data, (err) => {
+      if(err) throw err;
+      console.log('Сделана запись в бандл\n');
+    });
+  });
+}
 
-copyDir();
+
+
+readDir();
+
